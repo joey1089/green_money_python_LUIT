15851099608167ef591c1601b925a073eab5b80c
Joseph Peter <josephvpeter7@gmail.com>
2022-10-29 17:33:15 -0400

    added new file matrix.py

---
+++ matrix.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3.10
+
+# 0 1 2
+# 3 4 5
+
+myMatrix = [[0,1,2], 
+            [3,4,5]]
+row_count = len(myMatrix)
+print(row_count)
+
+column_count = len(myMatrix[0])
+print(column_count)
+
+
+
