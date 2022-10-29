24130964077d045c939072992a9b2e0e83d8dc6f
Joseph Peter <josephvpeter7@gmail.com>
2022-10-29 09:24:37 -0400

    added runtime execution

--- string_manipulation.py
+++ string_manipulation.py
@@ -3,3 +3,30 @@ print(str_cons.capitalize())
 print(id(str_cons))
 print(id('test_this_out'))
 print(str_cons)
+print(str_cons[-1])
+print(str_cons[-2])
+print(str_cons[-3])
+print(str_cons[4])
+print(str_cons[0:2])
+#  >>> test_str = 'testing'
+# >>> test_str[2:]
+# 'sting'
+# >>> test_str[2:len(test_str)]
+# 'sting'
+# >>> test_str[0:3]
+# 'tes'
+# >>> test_str[1:3]
+# 'es'
+# >>> test_str[3:5]
+# 'ti'
+# >>> test_str[0:8]
+# 'testing'
+# >>> test_str[::-1]
+# 'gnitset'
+# >>> test_str[0::8:2]
+#   File "<stdin>", line 1
+#     test_str[0::8:2]
+#                  ^
+# SyntaxError: invalid syntax
+# >>> test_str[0:8:2]
+# 'tsig' 
