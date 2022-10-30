84b4c484e3e38381ae1e654bd96eba6080a7bb83
Joseph Peter <josephvpeter7@gmail.com>
2022-10-29 20:19:36 -0400

    added new files

---
+++ hackerank/capitalize.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(s):
+    # s = s.split(" ")
+    # s1 = s[0]
+    # s2 = s[1]
+    # s1 = str(s1).capitalize()
+    # s2 = str(s2).capitalize()
+    # s = s1+" "+s2
+   
+    return s.title()
+    
+
+name = "henry ford"
+print(solve(name))
