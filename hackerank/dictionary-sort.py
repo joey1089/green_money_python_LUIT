2c81a33271170cab2ba26e66a9b314f45f6f661b
Joseph Peter <josephvpeter7@gmail.com>
2022-11-02 16:43:06 -0400

    updated dictionary-sort.py

--- hackerank/dictionary-sort.py
+++ hackerank/dictionary-sort.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3.10
 if __name__ == '__main__':
     dict_item = {}
+    
     for i in range(int(input())):
         name = input()
         score = float(input())
         dict_item[name] = score
         
     new_alpha = dict_item.items()
-    print(new_alpha)    
-    new_val = sorted(new_alpha)
+    newdict_list = dict(new_alpha) # Convert tuple to dictionary
+    print("Prints the Tuple list : ",new_alpha)    
+    new_val = sorted(new_alpha) # sort the tuple list
+    sortedValues = sorted(newdict_list.items())
+    print(f"Sorted values : {sortedValues}")
+    #sortvalue = dict(sorted(newdict_list) for k,v in newdict_list)
     
     print(new_val)
+  
