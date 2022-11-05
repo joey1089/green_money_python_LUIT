b5827e62a2b9afe2ee5b625e64c0b9ea48751507
Joseph Peter <josephvpeter7@gmail.com>
2022-11-05 00:07:57 -0400

    updated code

--- project/ec2_name-generator.py
+++ project/ec2_name-generator.py
@@ -23,7 +23,7 @@ def clr_scrn():
 
 def generate_names(dept_names,num_of_ec2):
     '''This method prints the auto generates unique names of the given department'''
-    # Get the random characters and numbers - to get unique name.
+    # Gets the random characters and numbers - to generate unique name.
     password_list = []
     for char in range(0,8):
         password_list += random.choice(letters)
@@ -35,7 +35,6 @@ def generate_names(dept_names,num_of_ec2):
         gen_ec2_names = random.sample(password_list,10)
         str_gen_names = [str(i) for i in gen_ec2_names]
         convert_strec2 = dept_names+"_"+"".join(str_gen_names)
-        # print("Department EC2 Instance name : ",convert_strec2.replace(" ",""))
         str_list_names.append(convert_strec2.replace(" ",""))
         count += 1
     return str_list_names
@@ -43,29 +42,31 @@ def generate_names(dept_names,num_of_ec2):
 def verify_names(generate_ec2_names):
     '''This method verifies all the generated ec2 names are unique'''
     if len(set(generate_ec2_names)) == len(generate_ec2_names):
-        return True
+        print("All the generated EC2 Instances names are unique !")
     else:
-        return False
+        print("Not all generated EC2 Instances names are unique !!!")
 
 
 clr_scrn()
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+print("======================== EC2 Instances Name Generator =======================================")
 num_of_ec2 = int(input("How many EC2 instances are needed : "))
-dept_names = str(input("Choose your department type either - Marketing, Accounting or Fin Ops : ")).title()
-generate_ec2_names = generate_names(dept_names,num_of_ec2)  
-if verify_names(generate_ec2_names):
-    print("All the generated EC2 Instances names are unique ")
-else:
-    print("Not all are unique ")
+dept_names = str(input("Choose your department type either - 'Marketing', 'Accounting' or 'Fin Ops' : ")).title()
+generate_ec2_names = generate_names(dept_names,num_of_ec2) 
+ 
+
 if dept_names == 'Marketing':      
-    print("\n Marketing Department EC2 Names : ",generate_ec2_names)
+    print("\n Marketing Department EC2 Names : ",generate_names(dept_names,num_of_ec2) )
+    verify_names(generate_names(dept_names,num_of_ec2))
    
 elif dept_names == 'Accounting':
-    print("\n Accounting Department EC2 Names : ",generate_names(dept_names,num_of_ec2))
+    print("\n Accounting Department EC2 Names : ",generate_names(dept_names,num_of_ec2) )
+    verify_names(generate_ec2_names)
   
 elif dept_names == 'Fin Ops':
-    print("\n FinOps Department EC2 Names :",generate_names(dept_names,num_of_ec2))
+    print("\n FinOps Department EC2 Names :",generate_names(dept_names,num_of_ec2) )
+    verify_names(generate_ec2_names)
 
 else:
     print("You can't use Name Genertor for your department !")
