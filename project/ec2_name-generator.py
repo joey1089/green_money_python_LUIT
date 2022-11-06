3befaeb0dd1d56f89448fd88f7ef47dea75d4524
Joseph Peter <josephvpeter7@gmail.com>
2022-11-05 22:50:37 -0400

    corrected validations

--- project/ec2_name-generator.py
+++ project/ec2_name-generator.py
@@ -1,5 +1,6 @@
 # **EC2 Random Name Generator**
 import random
+import string
 import os
 
 def clr_scrn():
@@ -8,6 +9,19 @@ def clr_scrn():
         os.system('clear')
     else:
         os.system('cls')
+
+def get_menu(no_ec2):
+    dept_names = str(input("Choose your department, type either - 'Marketing', 'Accounting' or 'Fin Ops' : ")).title()
+
+    if dept_names == 'Marketing':      
+        print("\nMarketing Department EC2 Names : ",generate_names(dept_names,no_ec2) )
+    elif dept_names == 'Accounting':
+        print("\nAccounting Department EC2 Names : ",generate_names(dept_names,no_ec2) )
+    elif dept_names == 'Fin Ops':
+        print("\nFinOps Department EC2 Names : ",generate_names(dept_names,no_ec2) )
+    else:
+        print("You can't use Name Genertor for your department !")
+
 
 def generate_names(dept_name,no_of_ec2):
     '''This method prints the auto generates unique names of the given department'''
@@ -27,7 +41,7 @@ def generate_names(dept_name,no_of_ec2):
 def verify_names(list_names):
     '''This method verifies all the generated ec2 names are unique'''    
     if len(set(list_names)) == len(list_names):
-        print("All the generated EC2 Instances names are unique !")
+        print("\nAll the generated EC2 Instances names are unique !")
         return True
     else:
         return "Not all generated EC2 Instances names are unique !!!"
@@ -35,8 +49,8 @@ def verify_names(list_names):
 def pwdgen():
     '''This method returns password combined with letters and numbers.'''
     password_list = []
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    letters = string.ascii_letters    
+    numbers = string.digits
     for char in range(0,8):
         password_list += random.choice(letters)
     for char in range(0,8):
@@ -44,18 +58,18 @@ def pwdgen():
     return password_list
 
 clr_scrn()
-print("========================== EC2 Instances Name Generator =======================================")
-num_of_ec2 = int(input("How many EC2 instances are needed : "))
-dept_names = str(input("Choose your department, type either - 'Marketing', 'Accounting' or 'Fin Ops' : ")).title()
+print("\n========================== EC2 Instances Name Generator =======================================\n")
+count = 3
+while count != 0 :
+    num_of_ec2 = int(input("How many EC2 instances are needed : "))
+    if num_of_ec2 >= 1:
+        get_menu(num_of_ec2)
+        break
+    else:
+        print("Invaild value, can't proceed...")
+        count -= 1
+    
 
-if dept_names == 'Marketing':      
-    print("Marketing Department EC2 Names : ",generate_names(dept_names,num_of_ec2) )
-elif dept_names == 'Accounting':
-    print("Accounting Department EC2 Names : ",generate_names(dept_names,num_of_ec2) )
-elif dept_names == 'Fin Ops':
-    print("FinOps Department EC2 Names : ",generate_names(dept_names,num_of_ec2) )
-else:
-    print("You can't use Name Genertor for your department !")
 
 
 
